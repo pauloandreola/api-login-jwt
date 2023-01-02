405c9f7027136a28f5609e3e325e73423f4e0f7b
Paulo Andreola <pauloandreola@gmail.com>
2023-01-02 16:20:53 -0300

    feat: clean code

--- src/data-source.ts
+++ src/data-source.ts
@@ -12,10 +12,6 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-//   synchronize: true,
-//   logging: true,
   entities: [`${__dirname}/**/entities/*.{ts,js}`],
-//   subscribers: [],
   migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
-// 
 });
