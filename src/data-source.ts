c6169f21fb7944b7bec8d36805b6b7566899d681
Paulo Andreola <pauloandreola@gmail.com>
2023-02-21 23:04:04 -0300

    update: clean code comments

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,7 +3,7 @@ import 'reflect-metadata';
 
 import { DataSource } from "typeorm";
 
-const port = process.env.DB_PORT as number| undefined
+const port = process.env.DB_PORT as number | undefined;
 
 export const AppDataSource = new DataSource({
   type: "postgres",
